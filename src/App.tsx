8154a34ca5412a6b727382cd46ab9322b67caca5
sahdev50 <dxdestroyer007@gmail.com>
2025-05-14 15:23:22 +0100

    installed tailwindcss zustand and threejs

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ function App() {
   return (
     <>
       <div>
-        <h1 className="text-3xl font-bold underline">
+        <h1 className="text-3xl font-bold">
           Hello world!
         </h1>
       </div>
